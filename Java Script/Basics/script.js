6fc09866801dd0db1918639e2beb76eceb66eca7
Arpita Patra <arpitapatra595@gmail.com>
2025-10-14 11:50:53 +0530

    fundamentals

--- Java Script/Basics/script.js
+++ Java Script/Basics/script.js
@@ -1,6 +1,25 @@
 // const s=10;
 // console.log(s);
 
-const a=11;
-a=20;
-console.log(a);
+// const a=11;
+// a=20;
+// console.log(a);
+
+// console.log(a);
+// var a=20;   //pehele hi a print kra diya pta nhi h a kya h (hositing)
+// console.log(a);
+
+// let a ;
+// console.log(a);
+// a=20;
+
+// console.log(a);
+// let a=10;
+
+//primitive and non primitive datatype
+// type coercion
+
+// let a=20;
+// let b="20";
+// console.log(a == b); //type coercion
+// console.log(a === b);
