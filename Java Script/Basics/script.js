d936a2a48b7ea5537f0e5d01bbad46ec6c780021
Arpita Patra <arpitapatra595@gmail.com>
2025-10-16 14:02:33 +0530

    practice

--- Java Script/Basics/script.js
+++ Java Script/Basics/script.js
@@ -6,7 +6,7 @@
 // console.log(a);
 
 // console.log(a);
-// var a=20;   //pehele hi a print kra diya pta nhi h a kya h (hositing)
+// var a=20;   //pehele hi a print kra diya pta nhi h a kya h (hositing) let hoist nhi hota
 // console.log(a);
 
 // let a ;
@@ -41,6 +41,47 @@
 // ****
 // ***
 // **
-// *
+
+//  var sum=function(a,b){
+//      return a+b;
+//  };
+//  console.log(sum(2,3));
+
+// console.log(a+20);
+// const a=10;
+
+// const greeting=(username) => {
+//     console.log("hello !"+username);
+// };
+
+// greeting();
+
+// const sum=(num1,num2,...rest) => {
+//     console.log(num1);
+//     console.log(num2);
+//     console.log(rest);
+// }
+// sum(1,2,3,4,5,6,7,8,9,10);
+
+
+// let a = 5
+// let b = 10;
+// const arr = [2,3,4,5]
+// let sumArrr =a+ b;
+// for(i = 0; i < arr.length; i++) {
+//     sumArrr = sumArrr + arr[i];
+// }
+// console.log(sumArrr);
+
+
+// const sum=(num1,num2,...rest) => {
+//     let result=num1+num2;
+//     for(let i=0;i<rest.length;i++){
+//         result=result+rest[i];
+//     }
+//     console.log(result);
+    
+// };
+// sum(1,2,3,4,5,6,7,8,9,10);
 
 
